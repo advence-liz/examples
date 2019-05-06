59dd0db7022f3fd851a067ef326f63ff4562e213
李卓 <lizhuo@lizhuodeMacBook-Pro.local>
2019-05-06 22:49:59 +0800

    add: base

--- cookies/app.js
+++ cookies/app.js
@@ -3,13 +3,17 @@
  * both as a cookie and as a response string.
  */
 
-const Koa = require('koa');
-const app = module.exports = new Koa();
+const Koa = require('koa')
+const app = (module.exports = new Koa())
 
 app.use(async function(ctx) {
-  const n = ~~ctx.cookies.get('view') + 1;
-  ctx.cookies.set('view', n);
-  ctx.body = n + ' views';
-});
+  const n = ~~ctx.cookies.get('view') + 1
 
-if (!module.parent) app.listen(3000);
+  console.log(n, ctx.url)
+  // 加这个判断是为了排除 /favico的干扰，因为浏览器会自动发这个请求
+  if (ctx.url.includes('view')) ctx.cookies.set('view', n, { path: '/view' })
+
+  ctx.body = n + 'cookie'
+})
+
+if (!module.parent) app.listen(3001)
